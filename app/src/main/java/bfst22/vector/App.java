10efd52e7efd5db52d6208da1628fafc11df8d2e
Anton Vitus Sohl Andersen <anton.sohl@gmail.com>
2022-09-23 15:45:12 +0200

    added osm file

--- app/src/main/java/bfst22/vector/App.java
+++ app/src/main/java/bfst22/vector/App.java
@@ -6,7 +6,7 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        var model = new Model("data/danmark.osm.obj");
+        var model = new Model("data/Femoo.osm");
         new View(model, primaryStage);
     }
 }
